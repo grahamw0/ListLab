c162bb44708c88b94ac2024148427258b83f97ba
Will <grahamw0@students.rowan.edu>
2016-09-24 15:42:23 -0400

    Incremental testing

--- src/listLab/Driver.java
+++ src/listLab/Driver.java
@@ -15,7 +15,8 @@ public class Driver {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     Song song = new Song("Some RadioHead Bull", "Radiohead", 2.3f);
-    System.out.println(song.toString());
+    Song song2 = new Song("Some RadioHead Bull", "Radiohead", 2.1f);
+    System.out.println(song.equals(song2));
   }
 
 }
