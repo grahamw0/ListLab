b9e887cf32cba22c13d223bf58d07d7d3b85327d
Will <grahamw0@students.rowan.edu>
2016-09-27 21:19:45 -0400

    Testing swaps with one million entries

--- src/listLab/Driver.java
+++ src/listLab/Driver.java
@@ -3,6 +3,8 @@
  */
 package listLab;
 
+import java.util.Random;
+
 /**
  * @author grahamw0
  *
@@ -14,16 +16,19 @@ public class Driver {
    */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    Song song = new Song("Some RadioHead Bull", "Radiohead", 2.3f);
+    /*Song song = new Song("Some RadioHead Bull", "Radiohead", 2.3f);
     Song song2 = new Song("Some RadioHead Bull", "Radiohead", 2.1f);
-    System.out.println(song.equals(song2));
+    System.out.println(song.equals(song2));*/
     
-    ArrayMyList<Integer> test = new ArrayMyList<>(Integer.class);
-    for(int i = 0; i < 20; i++) {
+    Random rand = new Random(666);
+    
+    ArrayMyList<Long> test = new ArrayMyList<>(Long.class);
+    for(long i = 0; i < 1000000L; i++) {
       test.add(i);
     }
-    System.out.println(test.contains(17));
+    System.out.println(test.contains(17L));
     System.out.println(test.get(2));
+    test.swap(rand.nextInt(1000001), rand.nextInt(1000001));
   }
 
 }
