86295719d562c351ede8e5b79b5b87c114de6545
Will Graham <grahamw0@students.rowan.edu>
2016-09-29 13:29:31 -0400

    Implemented a bunch of methods

--- src/listLab/SingularLL.java
+++ src/listLab/SingularLL.java
@@ -11,12 +11,12 @@ package listLab;
 class Node {
   Object data;
   Node next;
-  
+
   public Node(Object data) {
     this.data = data;
     next = null;
   }
-  
+
   public Node(Object data, Node next) {
     this.data = data;
     this.next = next;
@@ -49,14 +49,261 @@ class Node {
   public void setNext(Node next) {
     this.next = next;
   }
-  
-  
+
+
 }
 
-public class SingularLL implements MyList<T> {
-  private int count;
+
+public class SingularLL<T> implements MyList<T> {
+  private int size;
   private Node head;
   private Node tail;
-  
-  
+
+  public SingularLL() {
+    size = 0;
+    head = null;
+    tail = null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#add(int, java.lang.Object)
+   * 
+   */
+  @Override
+  public boolean add(int index, T o) {
+    if (index < 0 || index > size - 1 || head == null) {
+      throw new IndexOutOfBoundsException();
+    }
+    Node newNode = new Node(o);
+    Node currentNode = head;
+    if (index == 0) {
+      newNode.setNext(head);
+      this.head = newNode;
+      size++;
+    } else {
+      for (int i = 0; i < index - 1 && currentNode.getNext() != null; i++) {
+        currentNode = currentNode.getNext();
+      }
+      newNode.setNext(currentNode.getNext());
+      currentNode.setNext(newNode);
+    }
+    size++;
+    return true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#add(java.lang.Object)
+   */
+  @Override
+  public boolean add(T o) {
+    if (size == 0) {
+      head = new Node(o);
+      tail = head;
+      size++;
+    } else {
+      tail.setNext(new Node(o));
+      tail = tail.getNext();
+      size++;
+    }
+    return true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#clear()
+   */
+  @Override
+  public boolean clear() {
+    head = null;
+    tail = null;
+    size = 0;
+    return true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#contains(java.lang.Object)
+   */
+  @Override
+  public boolean contains(T o) {
+    Node currentNode = head;
+    for (int i = 0; i < size; i++) {
+      if (currentNode.getData() == o) {
+        return true;
+      } else {
+        currentNode = currentNode.getNext();
+      }
+    }
+    return false;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#get(int)
+   */
+  @Override
+  public T get(int index) {
+    if (index < 0 || index > size - 1) {
+      throw new IndexOutOfBoundsException();
+    }
+    if (head == null) {
+      throw new NullPointerException(); // TODO Maybe custom exception???
+    }
+
+    Node currentNode = head;
+    for (int i = 0; i < index; i++) {
+      currentNode = currentNode.getNext();
+    }
+    return (T) currentNode.getData();
+
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#indexOf(java.lang.Object)
+   */
+  @Override
+  public int indexOf(T o) {
+    Node currentNode = head;
+    for (int i = 0; i < size - 1; i++) {
+      if (currentNode.getData() == o) {
+        return i;
+      }
+      currentNode = currentNode.getNext();
+    }
+    return -1;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#isEmpty()
+   */
+  @Override
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#remove(int)
+   */
+  @Override
+  public T remove(int index) {
+    if (index < 0 || index > size - 1) {
+      throw new IndexOutOfBoundsException();
+    }
+
+    if (head == null) {
+      throw new NullPointerException(); // Maybe custom exception???
+    }
+
+    Node currentNode = head;
+    Object deletedData;
+    if (index == size - 1) {
+      for (int i = 0; i < index - 1; i++) {
+        currentNode = currentNode.getNext();
+      }
+      deletedData = tail.getData();
+      tail = currentNode;
+      currentNode.setNext(null);
+      size--;
+    } else {
+      for (int i = 0; i < index - 1; i++) {
+        currentNode = currentNode.getNext();
+      }
+      deletedData = currentNode.getNext().getData();
+      currentNode.setNext(currentNode.getNext().getNext());
+      size--;
+    }
+    return (T) deletedData;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#remove(java.lang.Object)
+   */
+  @Override
+  public T remove(Object o) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#set(int, java.lang.Object)
+   */
+  @Override
+  public boolean set(int index, Object element) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#size()
+   */
+  @Override
+  public int size() {
+    return size;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#subList(int, int)
+   */
+  @Override
+  public MyList<T> subList(int fromindex, int toIndex) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#toArray()
+   */
+  @Override
+  public T[] toArray() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#swap(int, int)
+   */
+  @Override
+  public boolean swap(int position1, int position2) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see listLab.MyList#shift(int)
+   */
+  @Override
+  public boolean shift(int positions) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+
+
 }
