41cf47cb04bdcd9c9c43edc9012c5a11721c2e37
Will Graham <grahamw0@students.rowan.edu>
2016-10-01 13:15:36 -0400

    Created Playlist interface

---
+++ src/listLab/Playlist.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package listLab;
+
+import java.util.List;
+
+/**
+ * @author grahamw0
+ *
+ */
+public interface Playlist {
+  public String playlistName();
+  public boolean addSong(Song s);
+  public boolean addSongAt(Song s, int index);
+  public Song getSongAt(Song s, int index);
+  public List getList();
+  public boolean removeSong(Song s);
+  public int totalSongs();
+  public float playListTime();
+  public boolean isSongInPlaylsit(String name, String artist);
+  public void songsByArtist(String name);
+  public boolean addSongsFrom(Playlist p);
+  public boolean moveSong(Song song, int position);
+  public boolean moveAllSongs(int positions);
+  public boolean swapSongs(int position1, int position2);
+}
