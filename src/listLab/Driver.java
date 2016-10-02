db3221dcaa3f013ade66af08111568c1f7e48e17
Will <grahamw0@students.rowan.edu>
2016-10-01 21:52:20 -0400

    Testing 1st Playlist random add

--- src/listLab/Driver.java
+++ src/listLab/Driver.java
@@ -3,6 +3,7 @@
  */
 package listLab;
 
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -15,34 +16,28 @@ public class Driver {
    * @param args
    */
   public static void main(String[] args) {
-   /*Random rand = new Random(666);
+   Random rand = new Random(666);
 
-    ArrayMyList<Long> test = new ArrayMyList<>(Long.class);
-    for (long i = 0; i < 1000000L; i++) {
-      test.add(i);
-    }
+   ArrayPlaylist list = new ArrayPlaylist("Testing List");
+   for(int i = 0; i < 1000000; i++) {
+     Integer n = rand.nextInt();
+     Integer m = rand.nextInt();
+     list.addSong(new Song(n.toString(), m.toString(), rand.nextFloat()));
+   }
 
     long startTime = System.currentTimeMillis();
-    for (long i = 0; i < 5L; i++) {
-      //test.add(rand.nextInt(1000001), rand.nextLong());  // This test took 2780309 milliseconds
+    for (int i = 0; i < 500000; i++) {
+      Integer n = rand.nextInt();
+      Integer m = rand.nextInt();
+      list.addSongAt(new Song(n.toString(), m.toString(), rand.nextFloat()), rand.nextInt(1000001));  // This test took 2780309 milliseconds
       //test.get(rand.nextInt(100001)); // 8 milliseconds
       //test.swap(rand.nextInt(1000001), rand.nextInt(1000001));  //This test took 92 milliseconds
       //test.shift(rand.nextInt(2000001)-1000001);
-      test.shift(rand.nextInt(1000001));
+      //test.shift(rand.nextInt(1000001));
     }
     long stopTime = System.currentTimeMillis();
-    long elapsedTime = (stopTime - startTime);
-    System.out.println(elapsedTime);*/
-    
-    CircularLL<Integer> ll = new CircularLL<>();
-    for(int i = 0; i < 20; i++) {
-      ll.add(i);
-    }
-   ll.swap(2,14);
-    
-    for(Integer i: ll.toArray()) {
-      System.out.println(i);
-    }
+    double elapsedTime = (stopTime - startTime) * 1000;
+    System.out.println(elapsedTime + " seconds");
     
     
   }
